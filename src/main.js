d2bbd804977fe20c31bd9e8e9003cf23fc2489c5
neprostostas <neprostostas.gg@gmail.com>
2023-01-27 13:06:29 +0200

    connected to Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import VueGtag from "vue-gtag";
 
 const app = createApp(App);
 app.use(router);
+app.use(VueGtag, {
+    appName: 'artdom',
+    pageTrackerScreenviewEnabled: true,
+    config: {
+        id: "G-YFWG4Z1ZNX",
+    },
+    onReady () {
+        console.log("ready");
+    }
+}, router);
 app.mount('#app');
